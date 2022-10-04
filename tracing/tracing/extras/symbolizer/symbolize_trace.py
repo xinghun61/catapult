859caf7278144d694ae4a8d8fdb0595c0f643246
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 15:47:59 +0000

    Adding numeric_owner as keyword arguement

--- tracing/tracing/extras/symbolizer/symbolize_trace.py
+++ tracing/tracing/extras/symbolizer/symbolize_trace.py
@@ -1679,7 +1679,7 @@ def FetchAndExtractSymbolsMac(symbol_base_directory, version,
               if not is_within_directory(path, member_path):
                   raise Exception("Attempted Path Traversal in Tar File")
       
-          tar.extractall(path, members, numeric_owner) 
+          tar.extractall(path, members, numeric_owner=numeric_owner) 
           
       
       safe_extract(tar, symbol_sub_dir)
